89c58776a070ff30c5d0d7cd22b94d3a2d6e3d07
Min Suh <strangeloop89@gmail.com>
2020-10-03 17:57:52 +0900

    feat: array writeup

---
+++ arrays.ts
@@ -0,0 +1,26 @@
+// array inference
+const carMakers = ["ford", "tayota", "chevy"];
+
+const carMakers2: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+const carsByMake = [["f150"], ["corolla"], ["camero"]];
+
+// Help with inference when extracting values
+const carMaker = carMakers[0];
+const carMaker2 = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toLowerCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), "2020-10-10"];
+importantDates.push(new Date());
+importantDates.push(1000);
+importantDates.push("2030-10-10");
